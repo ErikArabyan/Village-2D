7bf7dd5eb1743312eb314541f0e76a61c94fe630
ErikArabyan <erik.arabyan@mail.ru>
2025-03-02 16:25:05 +0400

    Version 3.0

--- js/gamelib.js
+++ js/gamelib.js
@@ -81,9 +81,7 @@ class Sprite {
 		this.height = height * GameSettings.scale
 	}
 
-	draw() {
-		console.log(this.image);
-		
+	draw() {		
 		if (this.frameWidth) {
 			ctx.drawImage(this.image, this.frame, this.move, this.frameWidth, this.moveHeight, this.mapPosition.x, this.mapPosition.y, this.width, this.height)
 		} else if (this.width) {
